3243c33bfa2e1875816014f55528c2dd4507dafb
Manuel Salmeron <manuelsalmeroncerdan@gmail.com>
2025-01-21 22:12:00 +0100

    pequeña reorganización del código

--- app/src/main/java/com/buenhijogames/media3practica01/Pantalla.kt
+++ app/src/main/java/com/buenhijogames/media3practica01/Pantalla.kt
@@ -1,18 +1,20 @@
 package com.buenhijogames.media3practica01
 
 import android.content.Context
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.media3.exoplayer.ExoPlayer
 import android.net.Uri
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 
 @Composable
 fun BeepScreen() {
@@ -28,20 +30,13 @@ fun BeepScreen() {
 
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Column {
-            Button(onClick = {
-                // Reproducimos el sonido
-                sonar(context, exoPlayer, R.raw.sonido1)
-            }) {
+            Button(onClick = { sonar(context, exoPlayer, R.raw.sonido1) }) {
                 Text(text = "Sonido1")
             }
 
-                Button(onClick = {
-                    // Reproducimos el sonido
-                    sonar(context, exoPlayer, R.raw.sonido2)
-                }) {
-                    Text(text = "Sonido2")
-                }
-
+            Button(onClick = { sonar(context, exoPlayer, R.raw.sonido2) }) {
+                Text(text = "Sonido2")
+            }
         }
     }
 }
@@ -49,9 +44,9 @@ fun BeepScreen() {
 private fun sonar(
     context: Context,
     exoPlayer: ExoPlayer,
-    sonido: Int
+    sonido: Int,
 ) {
-    val uri = Uri.parse("android.resource://" + context.packageName + "/" + R.raw.sonido1)
+    val uri = Uri.parse("android.resource://" + context.packageName + "/" + sonido)
     val mediaItem = MediaItem.fromUri(uri)
     exoPlayer.setMediaItem(mediaItem)
     exoPlayer.prepare()
